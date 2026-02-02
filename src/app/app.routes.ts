ac6350dbe3e595eb27aac7286b16584cd6928c2a
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2026-02-02 18:21:56 +0800

    Save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadComponent: () => import('./default-layout/default-layout.component').then(m => m.DefaultLayoutComponent),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/landing/landing.component').then(m => m.LandingComponent),
+      },
+    ],
+  }
+];
---
+++ src/app/default-layout/default-footer/default-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-footer',
+  imports: [],
+  templateUrl: './default-footer.component.html',
+  styleUrl: './default-footer.component.scss',
+})
+export class DefaultFooterComponent {
+
+}
