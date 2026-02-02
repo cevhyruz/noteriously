ef9776abc58d04d7f57991958c61ee87645cfa68
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2026-02-02 21:32:20 +0800

    chore(deps): Update angular to v21.1.2

--- src/app/default-layout/default-header/default-header.component.ts
+++ src/app/default-layout/default-header/default-header.component.ts
@@ -1,11 +1,10 @@
 import { Component, input, inject, signal, computed } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-default-header',
   imports: [
-    CommonModule,
     RouterLink,
   ],
   templateUrl: './default-header.component.html',
