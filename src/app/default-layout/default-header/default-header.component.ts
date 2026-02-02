01c4f5f0d2a88678f206021a558fc586a68097e3
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2026-02-03 01:26:07 +0800

    update

--- src/app/default-layout/default-header/default-header.component.ts
+++ src/app/default-layout/default-header/default-header.component.ts
@@ -1,15 +1,22 @@
 import { Component, input, inject, signal, computed } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import {
+  HeaderComponent
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-default-header',
   imports: [
     RouterLink,
+    HeaderComponent,
   ],
   templateUrl: './default-header.component.html',
   styleUrl: './default-header.component.scss',
 })
-export class DefaultHeaderComponent {
+export class DefaultHeaderComponent extends HeaderComponent {
+
+  constructor() {
+    super();
+  }
 
 }
